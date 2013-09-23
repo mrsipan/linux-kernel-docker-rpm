e4b3d89fb26acdfecbb6e3da19252aaf71b40c2f
Ben Sanchez <mrsipan@gmail.com>
2013-09-23 16:46:31 -0400

    add initial files

---
+++ build-rpm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -vex
+
+# needed rpms
+#yum -y install ncurses-devel git gcc rpmbuild
+
+KERNEL_VERSION=3.10.10
+wget --no-check-certificate https://www.kernel.org/pub/linux/kernel/v3.x/linux-${KERNEL_VERSION}.tar.xz
+
+
+# get aufs user tools and build tar
+printf "build aufs3-standalone\n"
+rm -rf aufs3-standalone
+git clone git://git.code.sf.net/p/aufs/aufs3-standalone
+
+cd aufs3-standalone
+git checkout aufs3.10
+git archive aufs3.10 --prefix='aufs3-standalone-3.10/' --format=tar | gzip -9 > ../aufs3-standalone-3.10.tar.gz
+cd ..
+rm -rf aufs3-standalone
+
+rpmbuild -bs --nodeps --define "_sourcedir ." --define "_srcrpmdir ." kernel-docker.spec
+
+# either mock or rpmbuild
+
+build_dir="$(mktemp -d)"
+mkdir -p $build_dir/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
+rpmbuild --rebuild --define "_topdir ${build_dir}" kernel-${KERNEL_VERSION}-1.src.rpm
+
+mv $build_dir/RPMS/*.rpm .
